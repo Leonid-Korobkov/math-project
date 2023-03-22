0b874870d5da5686bd13a9b272bb6bf137f18045
Leonid-Korobkov <korobkov-leo@mail.ru>
2023-03-22 22:44:05 +0300

    исправлены ошибки

--- notation/notation.js
+++ notation/notation.js
@@ -9,37 +9,33 @@ function addErrorMessege(base) {
   errorMessage.innerHTML = `Допустимые символы: <span style="color: #47c527">${allowedChars.slice(0, base)}</span>`
   formButton.style.opacity = '0.3'
   formButton.style.pointerEvents = 'none'
-  form.number.style.cssText = `
-    outline: 1px solid #ff2c2c;
-    -webkit-box-shadow: 1px 0px 38px 0px rgba(255, 44, 44, 0.73); 
-    -moz-box-shadow: 1px 0px 38px 0px rgba(255, 44, 44, 0.73); 
-    box-shadow: 1px 0px 38px 0px rgba(255, 44, 44, 0.73);
-  `
+  form.number.classList.add('form-input__error')
 }
 function removeErrorMessege() {
   errorMessage.style.display = 'none'
   formButton.style.opacity = '1'
   formButton.style.pointerEvents = 'all'
-  // form.number.style.cssText = `
-  //   outline: none;
-  //   -webkit-box-shadow: none;
-  //   -moz-box-shadow: none;
-  //   box-shadow: none;
-  // `
+  form.number.classList.remove('form-input__error')
 }
 function validateNumberInput(number, base) {
-  const isBase = /^[0-9a-z]+$/i.test(number) && parseInt(number, base).toString(base) === number.toString().toLowerCase()
+  const isBase = /^[0-9a-z]*\.?[0-9a-z]*$/i.test(number) && parseFloat(number, base).toString(base) === number.toString().toLowerCase()
   if (!isBase) addErrorMessege(base)
   else removeErrorMessege()
+  return isBase
 }
 
+form.addEventListener('keydown', function (e) {
+  if (!validateNumberInput(isFinite(form.number.value) ? +form.number.value : form.number.value, +form.fromNum.value) && event.key === "Enter") {
+    e.preventDefault()
+  }
+})
 form.addEventListener('submit', function (e) {
   e.preventDefault()
   const number = isFinite(form.number.value) ? +form.number.value : form.number.value
   const numberFrom = +form.fromNum.value
   const numberTo = +form.toNum.value
 
-  const decimalNumber = parseInt(number, numberFrom)
+  const decimalNumber = parseFloat(number, numberFrom)
   const convertedNumber = decimalNumber.toString(numberTo)
 
   const stringToHtml = `
@@ -52,11 +48,9 @@ form.addEventListener('submit', function (e) {
     `
   resultBody.innerHTML = stringToHtml
 })
-
 form.number.addEventListener('input', function (e) {
   validateNumberInput(isFinite(form.number.value) ? +form.number.value : form.number.value, +form.fromNum.value)
 })
-
 form.fromNum.addEventListener('change', function (e) {
   validateNumberInput(isFinite(form.number.value) ? +form.number.value : form.number.value, +form.fromNum.value)
 })
